dcea7444d47fc2995292a2136d150f62654e4e24
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2024-10-29 17:20:15 -0400

    remove grid placeholders

--- src/components/Grid.svelte
+++ src/components/Grid.svelte
@@ -16,56 +16,6 @@
           </a>
         </li>
       {/each}
-
-      <!-- place holders -->
-      <li class="grid-menu-item">
-        <a href="/">
-          <img src="{assetPath}images/menu-pics/bobolink.jpg" alt="">
-          <h3>Bobolink</h3>
-        </a>
-      </li>
-      <li class="grid-menu-item">
-        <a href="/">
-          <img src="{assetPath}images/menu-pics/blackcappedchickadee.jpg" alt="">
-          <h3>Black-capped Chickadee</h3>
-        </a>
-      </li>
-      <li class="grid-menu-item">
-        <a href="/">
-          <img src="{assetPath}images/menu-pics/barnowl.jpg" alt="">
-          <h3>Barn Owl</h3>
-        </a>
-      </li>
-      <li class="grid-menu-item">
-        <a href="/">
-          <img src="{assetPath}images/menu-pics/americanredstart.jpg" alt="">
-          <h3>American Redstart</h3>
-        </a>
-      </li>
-      <li class="grid-menu-item">
-        <a href="/">
-          <img src="{assetPath}images/menu-pics/crow.jpg" alt="">
-          <h3>Crow</h3>
-        </a>
-      </li>
-      <li class="grid-menu-item">
-        <a href="/">
-          <img src="{assetPath}images/menu-pics/burrowingowl.jpg" alt="">
-          <h3>Burrowing Owl</h3>
-        </a>
-      </li>
-      <li class="grid-menu-item">
-        <a href="/">
-          <img src="{assetPath}images/menu-pics/bluejay.jpg" alt="">
-          <h3>Blue Jay</h3>
-        </a>
-      </li>
-      <li class="grid-menu-item">
-        <a href="/">
-          <img src="{assetPath}images/menu-pics/bobolink.jpg" alt="">
-          <h3>Bobolink</h3>
-        </a>
-      </li>
     </ul>
   </nav>
 </main>
